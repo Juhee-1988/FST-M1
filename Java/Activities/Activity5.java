e466e49fc1ddc046ba73941192e7ec80ff50f99f
Juhee-1988 <juhee.jameela.h.m@ibm.com>
2024-06-19 10:09:39 +0530

    Add files via upload

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,13 @@
+package Activities;
+
+public class Activity5 {
+	
+	public static void main(String[] args)
+	{
+		String title="Harry Porter";
+		Book newNovel=new MyBook();
+		newNovel.setTitle(title);
+		System.out.println("The title is : "+newNovel.getTitle());
+	}
+
+}
