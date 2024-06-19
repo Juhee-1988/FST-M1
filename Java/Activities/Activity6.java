54566967cc620aab39e87d7ab52d70044c639717
Juhee-1988 <juhee.jameela.h.m@ibm.com>
2024-06-19 10:42:45 +0530

    Add files via upload

---
+++ Java/Activities/Activity6.java
@@ -0,0 +1,18 @@
+package Activities;
+
+public class Activity6 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Plane plane=new Plane(10);
+		plane.onboard("Juhee");
+		plane.onboard("Ridha");
+		plane.onboard("Zara");
+		System.out.println("Plane took off at: "+plane.takeOff());
+		System.out.println("People on the plane: "+plane.getPassengers());
+		Thread.sleep(5000);
+		plane.land();
+		System.out.println("Plane landed at: "+plane.getLastTimeLanded());
+		System.out.println("People on the plane after landing: "+plane.getPassengers());
+	}
+
+}
