f4ba1ed5aad1551207cf767da4d971e29f9c23fe
Juhee-1988 <juhee.jameela.h.m@ibm.com>
2024-06-30 10:31:30 +0530

    Add files via upload

---
+++ SQL/Activities/Activity11.sql
@@ -0,0 +1,18 @@
+REM   Script: Activity11
+REM   Set Operators
+
+select customer_id, customer_name from customers a 
+where 1 < (select count(*) from orders b where a.customer_id=b.customer_id) 
+union 
+select salesman_id, salesman_name from salesman a 
+where 1 < (select count(*) from orders b where a.salesman_id=b.salesman_id) 
+order by customer_name;
+
+select a.salesman_id, a.salesman_name, b.order_no, 'highest on', b.order_date, b.purchase_amount from salesman a, orders b  
+    where a.salesman_id=b.salesman_id  
+    and b.purchase_amount = (select max(purchase_amount) from orders c where c.order_date = b.order_date) 
+union 
+select a.salesman_id, a.salesman_name, b.order_no, 'lowest on', b.order_date, b.purchase_amount from salesman a, orders b  
+    where a.salesman_id=b.salesman_id  
+    and b.purchase_amount = (select min(purchase_amount) from orders c where c.order_date = b.order_date);
+
