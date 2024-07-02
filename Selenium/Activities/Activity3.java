ce12c77ac263c78e200518c791e644714cff0e73
Juhee-1988 <juhee.jameela.h.m@ibm.com>
2024-07-02 09:37:20 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity3.java
@@ -0,0 +1,27 @@
+package com.example.fst_m1_selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity3 {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://v1.training-support.net");
+		
+		System.out.println("Home page title: "+ driver.getTitle());
+		
+		WebElement element = driver.findElement(By.xpath("//a[@id='about-link']"));
+		
+		element.click();
+		
+		System.out.println("About us page title: "+ driver.getTitle());
+				
+		driver.quit();
+		
+	}
+
+}
